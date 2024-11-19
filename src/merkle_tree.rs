388b6f7142b0a1bc04e2965ff6dc8ee8e7516add
Leandro Serra <leandro.serra@lambdaclass.com>
2024-11-19 12:59:45 -0300

    Add logic to add an element to the tree

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -62,4 +62,66 @@ impl MerkleTree {
         }
         next_layer
     }
+
+    pub fn add_element<T: std::convert::AsRef<[u8]>>(&mut self, elem: T) {
+        let mut hasher = Sha3_256::new();
+        let Some(last_layer) = self.layers.last_mut() else {
+            return;
+        };
+        hasher.update(elem);
+        last_layer.push(hasher.finalize_reset().into());
+
+        // The element is the first on the tree
+        if last_layer.len() == 1 {
+            return;
+        }
+        // We need to create the root layer
+        if last_layer.len() == 2 {
+            hasher.update(last_layer[0]);
+            hasher.update(last_layer[1]);
+            self.layers.insert(0, vec![hasher.finalize().into()]);
+            return;
+        }
+        // We have 2 or more layers
+        let mut prev_idx = self.layers.len() - 1;
+        let mut curr_idx = self.layers.len() - 2;
+
+        loop {
+            let previous_layer = self.layers[prev_idx].clone();
+            let current_layer = &mut self.layers[curr_idx];
+            let previous_layer_elem_count = previous_layer.len();
+
+            if (previous_layer.len() - 1) % 2 == 0 {
+                // Duplicate left node
+                hasher.update(previous_layer[previous_layer_elem_count - 1]);
+                hasher.update(previous_layer[previous_layer_elem_count - 1]);
+                current_layer.push(hasher.finalize_reset().into());
+            } else {
+                hasher.update(previous_layer[previous_layer_elem_count - 2]);
+                hasher.update(previous_layer[previous_layer_elem_count - 1]);
+                let Some(last_element) = current_layer.last_mut() else {
+                    panic!();
+                };
+                *last_element = hasher.finalize_reset().into();
+            }
+
+            if curr_idx > 0 {
+                curr_idx -= 1;
+            } else {
+                break;
+            }
+            prev_idx -= 1;
+        }
+
+        let Some(first_layer) = self.layers.first() else {
+            panic!()
+        };
+
+        // We need to create the last layer
+        if first_layer.len() != 1 {
+            hasher.update(first_layer[0]);
+            hasher.update(first_layer[1]);
+            self.layers.insert(0, vec![hasher.finalize().into()]);
+        }
+    }
 }
