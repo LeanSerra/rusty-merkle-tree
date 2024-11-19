8267a410b2cb194a5316865e33fe8fcb1dcc7041
Leandro Serra <leandro.serra@lambdaclass.com>
2024-11-19 12:59:45 -0300

    Add logic to add an element to the tree

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ mod merkle_tree;
 use merkle_tree::MerkleTree;
 
 fn main() {
-    let tree = MerkleTree::from_leaves(&["1", "2", "3", "4"]);
-    tree.print_layers();
+    let tree = MerkleTree::from_leaves(&["1", "2", "3", "4", "5"]);
+    println!("tree with 4 and 5");
+    println!("{tree}");
+    let mut tree = MerkleTree::from_leaves(&["1", "2", "3"]);
+    println!("Tree without 4 and 5");
+    println!("Before adding 4");
+    println!("{tree}");
+    tree.add_element("4");
+    println!("After adding 4 and before 5");
+    println!("{tree}");
+    tree.add_element("5");
+    println!("After adding 5");
+    println!("{tree}");
 }
